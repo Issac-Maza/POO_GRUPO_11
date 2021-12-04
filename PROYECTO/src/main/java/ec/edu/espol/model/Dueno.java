72f2175b17164b30625dc10d4a6c0a9b2036f17c
Issac-Maza <issamaza@espol.edu.ec>
2021-12-04 04:40:20 -0500

    Actualizando la clase persona y dueño, agregue la clase Util

--- PROYECTO/src/main/java/ec/edu/espol/model/Dueno.java
+++ PROYECTO/src/main/java/ec/edu/espol/model/Dueno.java
@@ -4,8 +4,13 @@
  * and open the template in the editor.
  */
 package ec.edu.espol.model;
-
+import ec.edu.espol.util.Util;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Objects;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -18,7 +23,13 @@ public class Dueno extends Persona{
     public  Dueno(int id, String nombres, String apellidos, String telefono, String email,String direccion, ArrayList<Mascota> mascotas){
         super(id,nombres,apellidos,telefono,email);
         this.direccion=direccion;
-        this.mascotas= new ArrayList();
+        this.mascotas= new ArrayList<>();
+    }
+    
+    public Dueno(int id, String nombres, String apellidos, String direccion){
+        super(id,nombres,apellidos);
+        this.direccion = direccion;
+        this.mascotas = new ArrayList<>();
     }
 
     public void setDireccion(String direccion) {
@@ -29,22 +40,27 @@ public class Dueno extends Persona{
         this.mascotas = mascotas;
     }
 
+    @Override
     public void setId(int id) {
         this.id = id;
     }
 
+    @Override
     public void setNombres(String nombres) {
         this.nombres = nombres;
     }
 
+    @Override
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    @Override
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+    @Override
     public void setEmail(String email) {
         this.email = email;
     }
@@ -53,30 +69,43 @@ public class Dueno extends Persona{
         return direccion;
     }
 
-    public ArrayList<Mascota> getMascotas() {
-        return mascotas;
+    @Override
+    public void saveFile(String File) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
+    @Override
     public int getId() {
         return id;
     }
 
+    @Override
     public String getNombres() {
         return nombres;
-    }
+}
 
+    @Override
     public String getApellidos() {
         return apellidos;
     }
 
+    @Override
     public String getTelefono() {
         return telefono;
     }
 
+    @Override
     public String getEmail() {
         return email;
     }
     
+    public static Dueno nextDueno(Scanner sc, String nomFile ){
+        nomFile = null;
+        int id_0 = Util.nextID(nomFile);
+        String nomb = sc.nextLine();
+        String nomb_0 = sc.nextLine();
+        return null;
+    }
     
     
 }
--- PROYECTO/src/main/java/ec/edu/espol/model/Persona.java
+++ PROYECTO/src/main/java/ec/edu/espol/model/Persona.java
@@ -5,6 +5,7 @@
  */
 package ec.edu.espol.model;
 import java.util.Objects;
+import java.util.Scanner;
 
 /**
  *
@@ -24,4 +25,81 @@ public abstract class Persona {
         this.telefono = telefono;
         this.email = email;
     }
+    public Persona(int id, String nombres, String apellidos){
+        this.id=id;
+        this.nombres=nombres;
+        this.apellidos=apellidos;
+        
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "Id:" + id + ", Nombres:" + nombres + ", apellidos=" + apellidos + ", telefono=" + telefono + ", email=" + email + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona pers = (Persona) obj;
+        return this.id == pers.id && Objects.equals(this.nombres, pers.nombres) && Objects.equals(this.apellidos, pers.apellidos);
+        
+ 
+    }
+    
+    
+    
+    public abstract void saveFile(String File);
+
 }
+    
+
